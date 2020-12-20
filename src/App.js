ebb5dacac348f13d0d912da480f3c447011bb618
Michele <lunatimichele@gmail.com>
2020-12-20 23:51:17 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "normalize.css";
+import "./App.scss";
+
+
+const App = () => {
+
+    return (
+        <div className="app-container">
+            <h1>HELLO STRIVERS</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from 'src/App';
+
+
+/*instanbul ignore next */
+ReactDOM.render(
+  <App />
+, document.getElementById('root'));
